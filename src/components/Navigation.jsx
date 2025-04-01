0dc8f310b6ab90d33f251b812255d6f9b050bcc3
Victoria <nicol_victoria@yahoo.com>
2025-04-01 11:34:40 +0200

    home

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -14,27 +14,31 @@ const Navigation = () => {
   return (
     <div className="navigation-bar">
       {/* Logo I moved outside navlinks to stay visible */}
-      <a href="/" className="logo-container">
-        <img className="logo" src={LogoMenu} alt="Travel Logo" style={{transition: 'transform 0.3s'}} onMouseOver={(e) => e.target.style.transform = 'scale(0.9)'} onMouseOut={(e) => e.target.style.transform = 'scale(1)'} />
+      <a href="./pages/Home" className="logo-container">
+        <img
+          className="logo"
+          src={LogoMenu}
+          alt="Travel Logo"
+          style={{ transition: "transform 0.3s" }}
+          onMouseOver={(e) => (e.target.style.transform = "scale(0.9)")}
+          onMouseOut={(e) => (e.target.style.transform = "scale(1)")}
+        />
       </a>
 
       {/* Navigation Links */}
       <ul className={`nav-links ${isMenuOpen ? "open" : ""}`}>
         {/* Left Menu */}
         <ul className="left-menu">
-          
           <li>
             <Link to="/destinations">DESTINATIONS</Link>
           </li>
           <li>
             <Link to="/TravelTips">TRAVEL TIPS</Link>
           </li>
-          
         </ul>
 
         {/* Right Menu */}
         <ul className="right-menu">
-          
           <li>
             <Link to="/support">SUPPORT</Link>
           </li>
